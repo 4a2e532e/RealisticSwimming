4aeadc66b937538b938e6cb39bcedeaeccd17944
4a2e532e <niemand.keiner0@gmail.com>
2016-05-22 20:56:53 +0200

    Fixed players getting dragged down while swimming when the fall animation is activated

--- src/realisticSwimming/RFallListener.java
+++ src/realisticSwimming/RFallListener.java
@@ -1,5 +1,6 @@
 package realisticSwimming;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,7 +15,7 @@ public class RFallListener implements Listener{
 		Player p = event.getPlayer();
 		//p.sendMessage(""+p.getFallDistance());
 		//p.sendMessage(""+p.getVelocity().getY());
-		if(p.getFallDistance()>RSMain.minFallDistance && RSMain.enableFall){
+		if(p.getFallDistance()>RSMain.minFallDistance && RSMain.enableFall && p.getLocation().getBlock().getType()!=Material.STATIONARY_WATER){
 			p.setGliding(true);
 		}
 	}
@@ -23,7 +24,7 @@ public class RFallListener implements Listener{
 	public void onEntityToggleGlideEvent(EntityToggleGlideEvent event){
 		if(event.getEntity() instanceof Player){
 			Player p = (Player) event.getEntity();
-			if(event.getEntity().getFallDistance()>0 && RSMain.enableFall){
+			if(event.getEntity().getFallDistance()>0 && RSMain.enableFall && p.getLocation().getBlock().getType()!=Material.STATIONARY_WATER && p.getLocation().subtract(0, 1, 0).getBlock().getType()!=Material.STATIONARY_WATER){
 				p.setVelocity(new Vector(0, -1, 0));
 				event.setCancelled(true);
 			}
