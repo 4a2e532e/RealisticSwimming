a5cfe52fb28fafc979d7a0b99c1f03d863ffa882
4a2e532e <jannes.saathoff@hotmail.de>
2017-02-08 23:11:46 +0100

    Fixed bug when stamina value is < 0

--- src/realisticSwimming/stamina/StaminaBar_Native.java
+++ src/realisticSwimming/stamina/StaminaBar_Native.java
@@ -30,6 +30,9 @@ public class StaminaBar_Native extends StaminaBar {
 
     @Override
     public void updateBar(float staminaValue) {
+        if(staminaValue < 0){
+            staminaValue = 0;
+        }
         staminaBar.setProgress(staminaValue/1000);
         setColor(staminaValue);
     }
